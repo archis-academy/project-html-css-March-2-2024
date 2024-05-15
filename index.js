da0b72ce60e22bf1d7d3d3979097a3b335d99c88
Macbook <macbook@192.168.1.33>
2024-05-15 22:35:40 +0300

    javascript kısmı düzeltilip active class yapısı kullanıldı

--- index.js
+++ index.js
@@ -8,38 +8,49 @@ const scrollOff = document.body;
 const locationNavbarBoxes = document.querySelectorAll(".package-location-navbar-box");
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const navbar = document.querySelector(".nav-links-box");
+const locationtpdetails = document.getElementById("tp-details");
+const locationtourplan=document.getElementById("location-page-id");
 
 for (let i = 0; i < locationNavbarBoxes.length; i++) {
- locationNavbarBoxes[i].addEventListener("click", () => {
-   for (let j = 0; j < locationNavbarBoxes.length; j++) {
-     locationNavbarBoxes[j].style.backgroundColor = "#f8f8f8";
-   }
-   locationNavbarBoxes[i].style.backgroundColor = "#fff";
- });
+  locationNavbarBoxes[i].addEventListener("click", (value) => {
+    for (let j = 0; j < locationNavbarBoxes.length; j++) {
+      locationNavbarBoxes[j].style.backgroundColor = "#f8f8f8";
+      if(value.target.parentElement.children[1].innerHTML=="Tour Plan"){
+        locationtpdetails.classList.remove("active");
+        locationtourplan.classList.add("active");
+
+      }
+      else if(value.target.parentElement.children[1].innerHTML=="Location"){
+        locationtpdetails.classList.add("active");
+        locationtourplan.classList.remove("active");
+      }
+
+  }
+  locationNavbarBoxes[i].style.backgroundColor = "#fff";
+  });
 }
 
 let isModalOpen = false;
 
 servicesLink.addEventListener("click", () => {
- if (!isModalOpen) {
-   dropDownLinks.style.display = "block";
-   isModalOpen = !isModalOpen;
-
- } else {
-   dropDownLinks.style.display = "none";
-   isModalOpen = !isModalOpen;
- }
+  if (!isModalOpen) {
+    dropDownLinks.style.display = "block";
+    isModalOpen = !isModalOpen;
+  } else {
+    dropDownLinks.style.display = "none";
+    isModalOpen = !isModalOpen;
+  }
 });
 
 
 let isNavbarOpen = false;
 
 hamburgerIcon.addEventListener("click", () => {
- if(!isNavbarOpen){
-   navbar.style.display = "flex";
-   isNavbarOpen = !isNavbarOpen;
- } else {
-   navbar.style.display = "none";
-   isNavbarOpen = !isNavbarOpen;
- }
+  if(!isNavbarOpen){
+    navbar.style.display = "flex";
+    isNavbarOpen = !isNavbarOpen;
+  } else {
+    navbar.style.display = "none";
+    isNavbarOpen = !isNavbarOpen;
+  }
 })
